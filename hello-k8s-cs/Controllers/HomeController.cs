7d74acf257e704c6a4f7a6e5099b9707fb1cef4a
Alex Wellnitz <alex@home.loc>
2020-04-27 14:21:46 +0200

    Projektdateien hinzufügen.

---
+++ hello-k8s-cs/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using hello_k8s_cs.Models;
+using System.Net;
+using System.Net.Sockets;
+
+namespace hello_k8s_cs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string machineName = Environment.MachineName;
+            string osName = Environment.OSVersion.VersionString;
+            string ipAdress = GetLocalIPAddress();
+
+            var tempObj = new { MachineName = machineName, OSName = osName, IPAdress = ipAdress };
+
+
+            return View(tempObj);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
---
+++ hello-k8s-cs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hello_k8s_cs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
